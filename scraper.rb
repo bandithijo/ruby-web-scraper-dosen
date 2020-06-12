b11dcf46f33ae9a18ed9960abb43d7eae6b50d14
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-06-13 02:45:45 +0800

    Create csv file

--- scraper.rb
+++ scraper.rb
@@ -10,7 +10,7 @@ def scraper
   dosen_listings = parsed_page.css('div.elementor-widget-wrap')
   dosen_listings.each do |dosen_list|
     dosen = {
-      nama_dosen: dosen_list.css("h2")[0]&.text,
+      nama_dosen: dosen_list.css("h2")[0]&.text&.gsub("\n", " "),
       nidn_dosen: dosen_list.css("h2")[1]&.text
     }
     if dosen[:nama_dosen] != nil
@@ -19,26 +19,13 @@ def scraper
   end
   # byebug
 
-  File.delete("daftar_dosen.html") if File.exist?("daftar_dosen.html")
-  File.open("daftar_dosen.html", "w") do |f|
-    f.puts '<!DOCTYPE html>'
-    f.puts '<html lang="en">'
-    f.puts '<head>'
-    f.puts '<meta charset="UTF-8">'
-    f.puts "<title>Daftar Dosen Universitas Mulia (#{dosens.count} dosen)</title>"
-    f.puts '<style>table,th,td{border:1px solid black;border-collapse:collapse;}</style>'
-    f.puts '</head>'
-    f.puts '<body>'
-    f.puts '<table>'
-    dosens.each do |dosen|
-      f.puts '<tr>'
-      f.puts "<td>#{dosen[:nama_dosen]}</td>"
-      f.puts "<td>#{dosen[:nidn_dosen]}</td>"
-      f.puts '</tr>'
+  File.delete("daftar_dosen.csv") if File.exist?("daftar_dosen.csv")
+  File.open("daftar_dosen.csv", "w") do |f|
+    f.puts 'no;nama_dosen;nidn_dosen'
+    dosens.each.with_index(1) do |dosen, index|
+      f.puts "#{index}\;#{dosen[:nama_dosen].strip}\;#{dosen[:nidn_dosen].strip}"
     end
-    f.puts '</table>'
-    f.puts '</body>'
-    f.puts '</html>'
+    f.close
   end
 
   puts "TOTAL DOSEN: #{dosens.count} orang"
