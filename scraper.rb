ac5db96d9abe997a9e26866484b2f66db5b1fb08
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-08-24 07:12:39 +0800

    Fix Ruby style with Rubocop on scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
-class Scraper
+# frozen_string_literal: true
 
+class Scraper
   attr_reader :parsed_page, :gender
   attr_writer :dosens
 
@@ -8,46 +9,41 @@ class Scraper
   end
 
   def fetch_all
-    dosens = Array.new
+    dosens = []
     dosen_listings = @parsed_page.css('div.elementor-widget-wrap p')
     dosen_listings[1..-2].each do |dosen_list|
-      nama_nidn_dosen = dosen_list&.text&.gsub(/(^\w.*?:)|(NIDN :\s)/, "").strip
+      nama_nidn_dosen = dosen_list&.text&.gsub(/(^\w.*?:)|(NIDN :\s)/, '')&.strip
       dosen = {
         nama_dosen: nama_nidn_dosen&.gsub(/[^A-Za-z.,’ ]/i, ''),
         nidn_dosen: nama_nidn_dosen&.gsub(/[^0-9]/i, '')
       }
 
-      if dosen[:nama_dosen] != nil
-        dosens << dosen
-      end
+      dosens << dosen unless dosen[:nama_dosen].nil?
     end
 
-    return dosens
+    dosens
   end
 
   def fetch_by_gender(gender)
-    if gender == 'pria'
-      index = 9
-    elsif gender == 'wanita'
-      index = 10
+    case gender
+    when 'pria' then index = 9
+    when 'wanita' then index = 10
     else
       puts 'Gender Not Qualified!'
     end
 
-    dosens = Array.new
+    dosens = []
     dosen_listings = @parsed_page.css('div.elementor-widget-wrap')[index].css('p')
     dosen_listings.each do |dosen_list|
-      nama_nidn_dosen = dosen_list&.text&.gsub(/(^\w.*?:)|(NIDN :\s)/, "").strip
+      nama_nidn_dosen = dosen_list&.text&.gsub(/(^\w.*?:)|(NIDN :\s)/, '')&.strip
       dosen = {
         nama_dosen: nama_nidn_dosen&.gsub(/[^A-Za-z., ]/i, ''),
         nidn_dosen: nama_nidn_dosen&.gsub(/[^0-9]/i, '')
       }
 
-      if dosen[:nama_dosen]
-        dosens << dosen
-      end
+      dosens << dosen unless dosen[:nama_dosen].nil?
     end
 
-    return dosens
+    dosens
   end
 end
