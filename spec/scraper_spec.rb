612cba0725583ff1f29894cbcc45c1cfe8d423ec
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-09-25 19:59:17 +0800

    Change specs by name to match by alphabet and numeric for nidn

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -20,15 +20,28 @@ describe Scraper do
   describe '.fetch_all' do
     context 'fetching all dosens data' do
       it 'returns array of dosens' do
-        expect(Scraper.new(parsed_page).fetch_all.class).to eq(Array)
+        expect(Scraper.new(parsed_page).fetch_all.class)
+          .to eq(Array)
       end
 
-      it 'returns first dosen name' do
-        expect(Scraper.new(parsed_page).fetch_all.first[:nama_dosen]).to eq('Abdul Fatah')
+      it 'returns first nama_dosen as alphabet' do
+        expect(Scraper.new(parsed_page).fetch_all.first[:nama_dosen])
+          .to match(/[a-zA-z\s\"]/)
       end
 
-      it 'returns last dosen name' do
-        expect(Scraper.new(parsed_page).fetch_all.last[:nama_dosen]).to eq('Zara Zerina Azizah, S.Pd.I, S.E., M.M')
+      it 'returns first nidn_dosen as numeric' do
+        expect(Scraper.new(parsed_page).fetch_all.first[:nidn_dosen])
+          .to match(/[0-9]/)
+      end
+
+      it 'returns last nama_dosen as alphabet' do
+        expect(Scraper.new(parsed_page).fetch_all.last[:nama_dosen])
+          .to match(/[a-zA-z\s\"]/)
+      end
+
+      it 'returns last nidn_dosen as numeric' do
+        expect(Scraper.new(parsed_page).fetch_all.last[:nidn_dosen])
+          .to match(/[0-9]/)
       end
     end
   end
@@ -36,31 +49,53 @@ describe Scraper do
   describe '.fetch_by_gender' do
     context 'fetching all dosens data by gender' do
       it 'returns array of dosens pria' do
-        expect(Scraper.new(parsed_page).fetch_by_gender('pria').class).to eq(Array)
+        expect(Scraper.new(parsed_page).fetch_by_gender('pria').class)
+          .to eq(Array)
       end
 
-      it 'returns first dosen pria name' do
+      it 'returns first nama_dosen pria as alphabet' do
         expect(Scraper.new(parsed_page).fetch_by_gender('pria').first[:nama_dosen])
-          .to eq('Abdul Fatah')
+          .to match(/[a-zA-z\s\"]/)
       end
 
-      it 'returns last dosen pria name' do
+      it 'returns first nidn_dosen pria as numeric' do
+        expect(Scraper.new(parsed_page).fetch_by_gender('pria').first[:nidn_dosen])
+          .to match(/[0-9]/)
+      end
+
+      it 'returns last nama_dosen pria as alphabet' do
         expect(Scraper.new(parsed_page).fetch_by_gender('pria').last[:nama_dosen])
-          .to eq('Zainal Arifin')
+          .to match(/[a-zA-z\s\"]/)
+      end
+
+      it 'returns last nidn_dosen pria as numeric' do
+        expect(Scraper.new(parsed_page).fetch_by_gender('pria').last[:nidn_dosen])
+          .to match(/[0-9]/)
       end
 
       it 'returns array of dosens wanita' do
-        expect(Scraper.new(parsed_page).fetch_by_gender('wanita').class).to eq(Array)
+        expect(Scraper.new(parsed_page).fetch_by_gender('wanita').class)
+          .to eq(Array)
       end
 
-      it 'returns first dosen wanita name' do
+      it 'returns first nama_dosen wanita as alphabet' do
         expect(Scraper.new(parsed_page).fetch_by_gender('wanita').first[:nama_dosen])
-          .to eq('Alisa Alaina, S.E., M.M')
+          .to match(/[a-zA-z\s\"]/)
       end
 
-      it 'returns last dosen wanita name' do
+      it 'returns first nidn_dosen wanita as numeric' do
+        expect(Scraper.new(parsed_page).fetch_by_gender('wanita').first[:nidn_dosen])
+          .to match(/[0-9]/)
+      end
+
+      it 'returns last nama_dosen wanita as alphabet' do
         expect(Scraper.new(parsed_page).fetch_by_gender('wanita').last[:nama_dosen])
-          .to eq('Zara Zerina Azizah, S.Pd.I, S.E., M.M')
+          .to match(/[a-zA-z\s\"]/)
+      end
+
+      it 'returns last nidn_dosen wanita as numeric' do
+        expect(Scraper.new(parsed_page).fetch_by_gender('wanita').last[:nidn_dosen])
+          .to match(/[0-9]/)
       end
     end
   end
